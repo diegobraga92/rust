d51a30e9fd1b83e5860a58489525d0f725de29c1
Diego Braga <diego.braga92@outlook.com>
2024-01-24 08:57:04 -0300

    Adding use

--- ch7/restaurant/src/lib.rs
+++ ch7/restaurant/src/lib.rs
@@ -46,10 +46,12 @@ mod back_of_house {
     }
 }
 
-pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
+use crate::front_of_house::hosting;
 
-    front_of_house::hosting::add_to_waitlist();
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+
+    hosting::add_to_waitlist();
 
     let mut meal = back_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Wheat");
