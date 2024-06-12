8f536a14cd2f8c9a898218be25fd02961bb21360
Diego Braga <dbraga@axcient.com>
2024-06-12 13:21:31 -0300

    Ch17, finish blog code

--- ch17/blog/src/main.rs
+++ ch17/blog/src/main.rs
@@ -6,9 +6,9 @@ fn main() {
     post.add_text("I ate a salad for lunch today");
     assert_eq!("", post.content());
 
-    port.request_review();
+    post.request_review();
     assert_eq!("", post.content());
 
-    port.approve();
+    post.approve();
     assert_eq!("I ate a salad for lunch today", post.content());
 }
