67e38d218d97fe2d11c2ab761e85abb6f44c3ca6
Diego Braga <dbraga@axcient.com>
2024-06-18 13:16:48 -0300

    Ch19, Unsafe code

---
+++ ch19/unsafe_code/src/main.rs
@@ -0,0 +1,47 @@
+unsafe fn dangerous() {}
+
+use std::slice;
+
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
+static HELLO_WORLD: &str = "Hello, world!";
+
+fn main() {
+
+    println!("name is: {HELLO_WORLD}");
+    
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+
+    unsafe {
+        dangerous();
+    }
+
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
+}
+
