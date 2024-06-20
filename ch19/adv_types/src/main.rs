1125e004e124172ec63f998db6f67cb5aaad4f93
Diego Braga <dbraga@axcient.com>
2024-06-20 12:13:14 -0300

    Ch19, Adv Types

---
+++ ch19/adv_types/src/main.rs
@@ -0,0 +1,34 @@
+type Kilometers = i32;
+
+impl<T> Option<T> {
+    pub fn unwrap(self) -> T {
+        match self {
+            Some(val) => val,
+            None => panic!("called `Option::unwrap()` on a `None` value"),
+        }
+    }
+}
+
+fn generic<T: ?Sized>(t: &T) {
+    // --snip--
+}
+
+fn main() {
+    
+    let x: i32 = 5;
+    let y: Kilometers = 5;
+
+    println!("x + y = {}", x + y);
+
+    type Thunk = Box<dyn Fn() + Send + 'static>;
+
+    let f: Thunk = Box::new(|| println!("hi"));
+
+    fn takes_long_type(f: Thunk) {
+        // --snip--
+    }
+
+    fn returns_long_type() -> Thunk {
+        // --snip--
+    }
+}
