01ec7704825d789500e810b522ff6443f87e0e0b
Diego Braga <diego.braga92@outlook.com>
2025-01-21 21:50:26 -0300

    Server basis

--- ZeroToProd/zero2prod/src/main.rs
+++ ZeroToProd/zero2prod/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 2)]
+async fn main() -> std::io::Result<()> {
+    run().await
 }
